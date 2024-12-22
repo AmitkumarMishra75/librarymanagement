f444520022ca3c34908cc3f2c7e11d879bcd4f05
Amit Kumar Mishra <amitbikky11@gmail.com>
2024-12-22 11:35:25 +0530

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Books
+
+# Register your models here.
+@admin.register(Books)
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['id','name','author','price','genre','quantity','rating']
+    list_display_links = ['author']
+
